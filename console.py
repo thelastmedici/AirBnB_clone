eeb9e404ff57f5f2569138e5f50ac9d8cf701636
thelastmedici <christianopeyemijoshua@gmail.com>
2024-03-20 23:16:01 -0400

    added the create command to console application

--- console.py
+++ console.py
@@ -2,24 +2,40 @@
 """import all necesarry modules"""
 
 import cmd
+from models.engine import storage
 
 class HBNBCommand(cmd.Cmd):
+
     """HBNBCommand class"""
 
     prompt = '(hbnb)'
 
     def emptyline(self):
-        """overrides default empty line behaviour so no command is executes"""
+        """opip install pycodestyleverrides default empty line behaviour so no command is executes"""
         pass
 
     def do_quit(self, line):
         """quit command to exit the program"""
         return True
-    
+
     def do_EOF(self, line):
         """EOF command to exit the program"""
         print()
         return True
-    
+
+    def do_create(self, line):
+        '''Function to create a new instance of a class'''
+        if line != "" and line is not None:
+             print("** class name missing **")
+        elif line not in storage.classes():
+                print("** class doesn't exist **")
+        else:
+             i = storage.classes()[line]()
+             i.save()
+             print(i.id)
+
+
+
 if __name__ == '__main__':
+
     HBNBCommand().cmdloop()
--- models/engine/base_model.py
+++ models/engine/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 import datetime
+from models import storage
 
 """import all necesarry modules"""
 
@@ -20,19 +21,21 @@ class BaseModel:
                     if name == 'created_at' or name == 'updated_at':
                         value = datetime.datetime.strptime(
                             value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, name, value) 
+                    setattr(self, name, value)
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.datetime.now()
+        storage.new(self)
 
     def __str__(self):
         """A string representation of BaseModel"""
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, self.__dict__)
-    
+
     def save(self):
         """update updated_at with the current datetime"""
+        storage.save()
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
